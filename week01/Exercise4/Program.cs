1e68b42d7d635000922d9f3cff83f054392437d2
domkay <domkayombo@gmail.com>
2026-03-14 20:08:29 +0100

    exercice 4 completed

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,65 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        List<int> numbers = new List<int>();
+        int userNumber = -1;
+        //COLLECTING INPUT ---
+        while (userNumber != 0)
+        {
+            Console.Write("Enter number: ");
+            string response = Console.ReadLine();
+            userNumber = int.Parse(response);
+            if (userNumber != 0)
+            {
+                numbers.Add(userNumber);
+            }
+        }
+        //COMPUTE SUM
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+        Console.WriteLine($"The sum is: {sum}");
+        //COMPUTE AVERAGE
+        double average = ((double)sum) / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+        //FIND THE MAXIMUM
+        int max = numbers[0];
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+        Console.WriteLine($"The largest number is: {max}");
+        //MALLEST POSITIVE NUMBER
+        int smallestPositive = int.MaxValue; 
+        
+        foreach (int number in numbers)
+        {
+            if (number > 0 && number < smallestPositive)
+            {
+                smallestPositive = number;
+            }
+        }
+        if (smallestPositive != int.MaxValue)
+        {
+            Console.WriteLine($"The smallest positive number is: {smallestPositive}");
+        }
+
+        //SORT AND DISPLAY
+        numbers.Sort(); 
+        Console.WriteLine("The sorted list is:");
+        foreach (int number in numbers)
+        {
+            Console.WriteLine(number);
+        }
     }
 }
