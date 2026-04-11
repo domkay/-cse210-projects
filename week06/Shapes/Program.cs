0669f3b36e9230ad91c82ea8f98d0eca0ab07e67
domkay <domkayombo@gmail.com>
2026-04-12 00:49:06 +0100

    Shape assignment

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -1,9 +1,29 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        // You know, the cool thing here is that this list can hold ANY shape.
+        List<Shape> shapes = new List<Shape>();
+
+        // Adding our different shapes to the same list
+        shapes.Add(new Square("Red", 4));
+        shapes.Add(new Rectangle("Blue", 5, 10));
+        shapes.Add(new Circle("Green", 3));
+
+        Console.WriteLine("Shape Areas and Colors:");
+        Console.WriteLine("-----------------------");
+
+        foreach (Shape s in shapes)
+        {
+            // This is polymorphism! The program knows which 'GetArea' to call
+            // even though 's' is just treated as a generic Shape here.
+            string color = s.GetColor();
+            double area = s.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area:F2}.");
+        }
     }
 }
---
+++ week06/Shapes/Shape.cs
@@ -0,0 +1,24 @@
+using System;
+
+public abstract class Shape
+{
+    private string _color;
+
+    public Shape(string color)
+    {
+        _color = color;
+    }
+
+    public string GetColor()
+    {
+        return _color;
+    }
+
+    public void SetColor(string color)
+    {
+        _color = color;
+    }
+
+    // Abstract method: no body here, but every child MUST implement it.
+    public abstract double GetArea();
+}
